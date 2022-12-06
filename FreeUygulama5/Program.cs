0914c602365126c31357e8781a7ce233257dad6b
Furkan Ertekin <furkanertekin0@gmail.com>
2022-12-06 22:56:03 +0300

    first commit

---
+++ FreeUygulama5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeUygulama5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int saat = 8;
+            int dakika = 30;
+            Console.WriteLine("Hoşgeldiniz");
+            Console.WriteLine(" ");
+            Console.Write("Lütfen 100 ile 500 arasında bir değer giriniz : ");
+            string kullaniciDeger=Console.ReadLine();   
+            int kullaniciDegerInt=Convert.ToInt32(kullaniciDeger);
+            int bolum = kullaniciDegerInt / 60;
+            saat = saat + bolum;
+            int bolum2 = kullaniciDegerInt % 60;
+            dakika = dakika + bolum2;
+
+            if (dakika>60)
+            {
+                bolum =dakika / 60;
+                saat = saat + bolum;             
+                dakika = dakika - (60*bolum);
+            }
+
+            if (100<=kullaniciDegerInt && kullaniciDegerInt<=500)
+            {      
+                    Console.WriteLine("Sınavın başlangıç saati 8:30'dur {0} dakika sonra saat {1}:{2} olacaktır.", kullaniciDegerInt,saat,dakika );
+                    Console.ReadLine();        
+            }
+
+            else
+            {
+                Console.WriteLine("Lütfen belirtilen koşullara uygun giriş yapınız!");
+                Console.ReadLine(); 
+            }
+            Console.ReadLine();
+        }
+    }
+}
